07a0bef0fb481034df5bd7c7fcaaeff8fd58e378
Yama <yama.samoila@nordnet.se>
2025-12-03 22:25:13 +0100

    add tests for seb

---
+++ internal/app/crawler/testdata/seb_main.js
@@ -0,0 +1,8 @@
+// Mock SEB JS bundle
+(function(){
+    const config = {
+        "apiUrl": "https://pricing-portal-api-public.clouda.sebgroup.com",
+        "x-api-key":"test-api-key-12345"
+    };
+    window.SEBConfig = config;
+})();
